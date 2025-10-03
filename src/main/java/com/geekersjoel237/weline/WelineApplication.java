b027d4c6c9f1765d0811829a1f159817425ecbbe
Geekers-Joel237 <79468070+Geekers-Joel237@users.noreply.github.com>
2025-10-03 23:17:34 +0100

    [config] feature: initial setup

---
+++ src/main/java/com/geekersjoel237/weline/WelineApplication.java
@@ -0,0 +1,13 @@
+package com.geekersjoel237.weline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WelineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/geekersjoel237/weline/WelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geekersjoel237.weline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
